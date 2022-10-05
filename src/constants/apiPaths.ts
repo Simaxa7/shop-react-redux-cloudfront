2dd11e71e5f379552c9d40aaf89629f5804f34a1
Pavel_Simashchanka <simaxa7@mail.ru>
2022-10-06 01:15:02 +0300

    changes for task5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://5124g6qam4.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
 };
