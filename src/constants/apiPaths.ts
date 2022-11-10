e9258261150ebf3169d1a6cf76b50dc12baf70f0
Pavel_Simashchanka <simaxa7@mail.ru>
2022-11-10 13:54:45 +0200

    changes regarding region and names

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
+  product: "https://ck8fhw4k1a.execute-api.eu-west-3.amazonaws.com/dev",
   order: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://5124g6qam4.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://atugtpey38.execute-api.eu-west-3.amazonaws.com/dev",
   bff: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
 };
