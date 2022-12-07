d5e61c15e61693c333c38393de4384dae6258c98
Pavel_Simashchanka <simaxa7@mail.ru>
2022-12-07 21:16:29 +0200

    changes task7 auth change APIs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://ck8fhw4k1a.execute-api.eu-west-3.amazonaws.com/dev",
-  order: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://atugtpey38.execute-api.eu-west-3.amazonaws.com/dev",
-  bff: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://4w297q7cpe.execute-api.eu-central-1.amazonaws.com/dev",
+  order: "https://ck8fhw4k1a.execute-api.eu-west-3.amazonaws.com/dev",
+  import: "https://yestgzy5kk.execute-api.eu-west-3.amazonaws.com/dev",
+  bff: "https://ck8fhw4k1a.execute-api.eu-west-3.amazonaws.com/dev",
+  cart: "https://4w297q7cpe.execute-api.eu-west-3.amazonaws.com/dev",
 };
 
 export default API_PATHS;
